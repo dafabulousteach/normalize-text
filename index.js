d02b369c81e209a1299caa039edb3acb3f077096
dafabulousteach <dafabulousteach@gmail.com>
2021-04-22 10:56:50 -0700

    adds more unit tests to check normalize functions adds function to render html for paragraphs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
  * turning URLs in text into links that go to that url
  */
 
-// document check function
+// Normalize the docuement object
 exports.checkDoc = (obj) => {
     let doc_children = obj.children;
     let newDoc = {
@@ -15,39 +15,110 @@ exports.checkDoc = (obj) => {
     };
 
     for(let i = 0; i < doc_children.length; i++){
-        // if I see two lists in a row, call combineList and combineBlockQuote on that index
-        if(doc_children[i].type === 'list'){
-            for(let j = i+1; j < doc_children.length; j++){
-                if(doc_children[j].type === 'list'){
-                    newDoc.children.push(this.combineList(doc_children[i], doc_children[j].children))
+
+        // check if there are two lists or two blockquotes adjacent to one another
+        if(doc_children[i].type === 'list' || doc_children[i].type === 'blockquote'){
+           // check if the child is at the end of the document object
+           if(doc_children[i].children.length === 0){
+               return newDoc;
+           } 
+           // check if the child has a link type
+           if(doc_children[i].children.type === 'link') {
+               if(doc_children[i].children.content === ""){
+                return newDoc;
+               }
+           }
+           else 
+           // if the child is not at the end of the document object
+            if((i+1) !== doc_children.length) {
+                for(let j = (i+1); j < doc_children.length; j++){
+                    // call combine function to combine adjacent lists or blockquotes
+                    newDoc.children.push(this.combine(doc_children[i], doc_children[j]));
+                    i++
                 }
             }
-        } else {
+            else {
+                newDoc.children.push(doc_children[i]);
+            }
+        } 
+        else {
             newDoc.children.push(doc_children[i]);
         }
     }
     // return the new object
     return newDoc;
-    // call renderHTML
-
 }
-exports.combineList = (firstList, secondList) => {
-    // first loop goes to through the documents children
-    for(let i = 0; i < secondList.length; i++){
-        // add the child to new object [list-item or the paragraph]
-        firstList.children.push(secondList[i]);
+exports.combine = (firstList, secondList) => {
+    if(secondList.type === 'list'){ 
+        firstList.children.push(secondList.children[0]);
+        return firstList;
+    } else if(secondList.type === "blockquote"){
+        firstList.children[0].children.push(secondList.children[0].children[0]);
+        return firstList;
     }
     return firstList;
 }
 
-exports.combineBlocks = () => {
 
-}
+exports.renderHtml = (obj) => {
+    let email = '';
+    let top_tag = '<article class="email">';
+    let bottom_tag = '</article>';
+    email = top_tag;
 
-exports.remove = () => {
+    let tags = [
+        {
+            type: 'paragraph',
+            start_tag: '<p>',
+            end_tag: '</p>',
+            link_start_tag: '<a href=',
+            link_middle_tag:'>', 
+            link_end_tag: '</a>'
+        },
+        {
+            type: 'list',
+            start_tag: '<ol>',
+            end_tag: '</ol>',
+            list_item_start_tag: '<li>',
+            list_item_end_tag: '</li>'
+        },
+        {
+            type: 'blockquote',
+            start_tag: '<blockquote>',
+            end_tag: '</blockquote>',
+            p_start_tag: '<p>',
+            p_end_tag: '</p>'
+        }
+    ]
 
-}
+    let doc_child = obj.children
 
-exports.renderHtml = () => {
+    for(let i = 0; i < doc_child.length; i++){
+        // find the matching tag
+        tags.filter((parent) => {
+            if(parent.type === doc_child[i].type){
+                // apply the parent to the child type
+                email += parent.start_tag
+                // check if the doc_children have children
+                if(doc_child[i].children){
+                    // loop through the children
+                    for(let j = 0; j < doc_child[i].children.length; j++){
+                        // if the child is a link
+                        if(doc_child[i].children[j].type === 'link'){
+                            // build the link html
+                            email += parent.link_start_tag + '"' + doc_child[i].children[j].url + '"'
+                            + parent.link_middle_tag + doc_child[i].children[j].content + parent.link_end_tag;
+                        } else{
+                            email += doc_child[i].children[j].content;
+                        }
+                    }                
+                    email += parent.end_tag
+                }
+                
+            }        
+        })        
+    }
+    
+    return email + bottom_tag;
 
 }
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { combine, remove, checkDoc } = require('./index.js')
+const { combine, remove, checkDoc, renderHtml } = require('./index.js')
 describe('Normalizing Input', () => {
     
     test('should combine adjacent lists', () => {
@@ -134,6 +134,147 @@ describe('Normalizing Input', () => {
             ]
         };
         expect(checkDoc(obj)).toEqual(result);
+    })
 
+    test('should handle block elements that are already properly normalized', () => {
+        let obj = {
+            type: "document",
+            children: [
+                {
+                    type: "paragraph",
+                    children: [
+                        { type: "text", content: "This is a paragraph, with a " },
+                        { type: "link", content: "link in it", url: "https://example.com" },
+                        { type: "text", content: "." }
+                    ]
+                },
+                {
+                    type: "blockquote",
+                    children: [
+                        {
+                            type: "paragraph",
+                            children: [
+                                { type: "text", content: "This text is quoted." },
+                                { type: "text", content: "This text is quoted." },
+                            ]
+                        }
+                    ]
+                }
+            ]
+        
+        }
+
+        let result = {
+            type: "document",
+            children: [
+                {
+                    type: "paragraph",
+                    children: [
+                        { type: "text", content: "This is a paragraph, with a " },
+                        { type: "link", content: "link in it", url: "https://example.com" },
+                        { type: "text", content: "." }
+                    ]
+                },
+                {
+                    type: "blockquote",
+                    children: [
+                        {
+                            type: "paragraph",
+                            children: [
+                                { type: "text", content: "This text is quoted." },
+                                { type: "text", content: "This text is quoted." },
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+
+        expect(checkDoc(obj)).toEqual(result);
+    })
+
+    test('should remove lists with no items in them', () => {
+        let obj = {
+            type: "document",
+            children: [
+                {
+                    type: "paragraph",
+                    children: [
+                        { type: "text", content: "This is a paragraph, with a " },
+                        { type: "link", content: "link in it", url: "https://example.com" },
+                        { type: "text", content: "." }
+                    ]
+                },
+                {
+                    type: "list",
+                    children: [],
+                }
+            ]
+        }
+        let result = {
+            type: "document",
+            children: [
+                {
+                    type: "paragraph",
+                    children: [
+                        { type: "text", content: "This is a paragraph, with a " },
+                        { type: "link", content: "link in it", url: "https://example.com" },
+                        { type: "text", content: "." }
+                    ]
+                }
+            ]
+        }
+        expect(checkDoc(obj)).toEqual(result);
+    })
+
+
+})
+
+describe('Render HTML', () => {
+    test('should render a normalized paragraph into HTML', () => {
+        let doc = {
+            type: "document",
+            children: [
+                {
+                    type: "paragraph",
+                    children: [
+                        { type: "text", content: "This is a paragraph, with a" },
+                        { type: "link", content: "link in it", url: "https://example.com" },
+                        { type: "text", content: "." }
+                    ]
+                }
+            ]
+        }
+        
+        let html = '<article class=\"email\"><p>This is a paragraph, with a<a href="https://example.com">link in it</a>.</p></article>'
+        expect(renderHtml(doc)).toEqual(html);
+    });
+
+    test('should render a normalized nested list into HTML', () => {
+        let doc = {
+            type: "list",
+            children: [
+                {
+                    type: "list-item",
+                    children: [{ type: "text", content: "List item 1"}]
+                },
+                {
+                    type: "list-item",
+                    children: [{ type: "text", content: "List item 2"}]
+                },
+                {
+                    type: "list",
+                    children: [
+                        {
+                            type: "list-item",
+                            children: [{ type: "text", content: "In a nested list"}]
+                        },
+                    ],
+                },
+            ],
+        }
+
+        let html = '<article class=\"email\"><ol><li>List item 1</li><li>List item 2</li><ol><li>In a nested list</li></ol></ol></article>';
+        expect(renderHtml(doc)).toEqual(html);
     })
 })
